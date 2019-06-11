05c475e7b9ffd1d18af0494abea8360d52de6fc8
Patrick Quirk <paquirk@microsoft.com>
2019-06-11 09:56:52 -0400

    Update models.py

--- scripts/templates/models.py
+++ scripts/templates/models.py
@@ -2,7 +2,7 @@
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
-# license information.
+# license information. Testing
 # --------------------------------------------------------------------------
 {%- for mod_api_version, _ in mod_to_api_version|dictsort %}
 from .{{ mod_api_version }}.models import *
